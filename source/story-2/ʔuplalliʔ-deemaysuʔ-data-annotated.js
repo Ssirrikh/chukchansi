f80d62ba77374e6c0aff2369994f33d8e123b3b2
ssirrikh <13358109+Ssirrikh@users.noreply.github.com>
2025-01-24 11:53:32 -0800

    finish ʔuplalliʔ-deemaysuʔ story, begin adding shelel'-shilithanna

---
+++ source/story-2/ʔuplalliʔ-deemaysuʔ-data-annotated.js
@@ -0,0 +1,146 @@
+
+let story_ʔuplalliʔ_deemaysuʔ = {
+	audio : `source/story-2/ʔuplalliʔ-deemaysuʔ-audio.mp3`,
+	titleENG : `Dove and Hummingbird Race Each Other`,
+	titleCHK : `ʔuplalliʔ ʔamaʔ yoʔ Deemaysuʔ Baanewsheʔ`,
+	titleShortENG : `Dove and Hummingbird`,
+	titleShortCHK : `ʔuplalliʔ Deemaysuʔ`,
+	timestampsBySentence : [
+		0.0,
+		`Wooyiʔ Yokuch’ wil’ wileʔ, Deemay’suʔ mich ʔayax hoy’no.`,
+		`Old time Indians would say, “Hummingbird was the fastest thing that flies.”`,
+		0.0,
+		`Hidʔan wil’ wileʔ, ʔuplalliʔ wakiy’ ʔayax Deemay’suʔun.`,
+		`Others would say, “Dove was faster than Hummingbird.”`,
+		0.0,
+		`Miʔin ʔaman baanelawshit ʔamaamig.`,
+		`So they made them race each other.`,
+		0.0,
+		`ʔuplalliʔ ʔamaʔ yoʔ Deemay’suʔ taanit baanewshich’ nootun’.`,
+		`Dove and Hummingbird went racing up north.`,
+		0.0,
+		`Juʔbaʔ yokuch’ taanit ʔamaa taʔashich’ wentaw.`,
+		`Some people went there to watch at the start.`,
+		0.0,
+		`Juʔbaʔ hidʔan taanit taxoolow taʔashich’ hayk’itaw.`,
+		`Some others went south to watch at the finish.`,
+		0.0,
+		`ʔuplalliʔ ʔamaʔ yoʔ Deemay’suʔ hoy’li hoynit.`,
+		`Dove and Hummingbird started flying right away.`,
+		0.0,
+		`Hoy’li Deemay’suʔ deeyit waʔ; miʔin daʔ moyinmi goosiʔnat.`,
+		`At first Hummingbird was far ahead, but eventually he tired and slowed up.`,
+		0.0,
+		`Nahniʔ ʔayax, miʔin goosiʔnat.`,
+		`Sometimes he was fast, sometimes slow.`,
+		0.0,
+		`ʔuplalliʔ wilch’init hoyinmeewet.`,
+		`Dove just kept flying.`,
+		0.0,
+		`Hoy’li ʔam hoyonhiy’ wuk’yut sipin’ miʔin yoʔ ʔadil’.`,
+		`At first his wings moved up and down.`,
+		0.0,
+		`Miʔin daʔ gidwiyat ʔam hoyonhiy’.`,
+		`Then his wings churned. (Like a paddlewheel.)`,
+		0.0,
+		`Sanum ʔuplalliʔ walxot ʔam baanewsha.`,
+		`Soon Dove overtook his rival.`,
+		0.0,
+		`Ch’awit ʔam.`,
+		`He won.`,
+		0.0,
+		`Deemay’suʔ daʔ waʔ dik’in.`,
+		`And Hummingbird was far behind.`,
+		0.0,
+		`Miʔin ʔaman hidyaʔ wilit, ʔuplalliʔ mich wakiy’ ʔayax hidyaʔan ch’enbayi.`,
+		`Then everybody said, “Dove is faster than all the birds.”`
+	],
+	timestampsByClause : [
+		[
+			4.0,
+			`Wooyiʔ Yokuch’ wil’ wileʔ,`,
+			`Old time Indians would say,`,
+			6.0,
+			`Deemay’suʔ mich ʔayax hoy’no.`,
+			`“Hummingbird was the fastest thing that flies.”`
+		],[
+			8.5,
+			`Hidʔan wil’ wileʔ,`,
+			`Others would say,`,
+			10.3,
+			`ʔuplalliʔ wakiy’ ʔayax Deemay’suʔun.`,
+			`“Dove was faster than Hummingbird.”`
+		],[
+			13.5,
+			`Miʔin ʔaman baanelawshit ʔamaamig.`,
+			`So they made them race each other.`
+		],[
+			16.8,
+			`ʔuplalliʔ ʔamaʔ yoʔ Deemay’suʔ taanit baanewshich’ nootun’.`,
+			`Dove and Hummingbird went racing up north.`
+		],[
+			22.0,
+			`Juʔbaʔ yokuch’ taanit ʔamaa taʔashich’ wentaw.`,
+			`Some people went there to watch at the start.`
+		],[
+			26.5,
+			`Juʔbaʔ hidʔan taanit taxoolow taʔashich’ hayk’itaw.`,
+			`Some others went south to watch at the finish.`
+		],[
+			31.8,
+			`ʔuplalliʔ ʔamaʔ yoʔ Deemay’suʔ hoy’li hoynit.`,
+			`Dove and Hummingbird started flying right away.`
+		],[
+			35.3,
+			`Hoy’li Deemay’suʔ deeyit waʔ;`,
+			`At first Hummingbird was far ahead,`,
+			38.5,
+			`miʔin daʔ moyinmi goosiʔnat.`,
+			`but eventually he tired and slowed up.`
+		],[
+			41.3,
+			`Nahniʔ ʔayax, miʔin goosiʔnat.`,
+			`Sometimes he was fast, sometimes slow.`
+		],[
+			44.7,
+			`ʔuplalliʔ wilch’init hoyinmeewet.`,
+			`Dove just kept flying.`
+		],[
+			48.0,
+			`Hoy’li ʔam hoyonhiy’ wuk’yut sipin’ miʔin yoʔ ʔadil’.`,
+			`At first his wings moved up and down.`
+		],[
+			52.7,
+			`Miʔin daʔ gidwiyat ʔam hoyonhiy’.`,
+			`Then his wings churned. (Like a paddlewheel.)`
+		],[
+			56.1,
+			`Sanum ʔuplalliʔ walxot ʔam baanewsha.`,
+			`Soon Dove overtook his rival.`
+		],[
+			60.0,
+			`Ch’awit ʔam.`,
+			`He won.`
+		],[
+			61.5,
+			`Deemay’suʔ daʔ waʔ dik’in.`,
+			`And Hummingbird was far behind.`
+		],[
+			64.0,
+			`Miʔin ʔaman hidyaʔ wilit,`,
+			`Then everybody said,`,
+			66.3,
+			`ʔuplalliʔ mich wakiy’ ʔayax hidyaʔan ch’enbayi.`,
+			`“Dove is faster than all the birds.”`
+		]
+	],
+	timestampsByWord : [
+		//
+	],
+	thumbnail : `source/story-2/ʔuplalliʔ-deemaysuʔ-thumbnail-small.jpg`,
+	images : [
+		0.0,
+		`source/story-2/ʔuplalliʔ-deemaysuʔ-thumbnail.jpg`
+	]
+};
+	
