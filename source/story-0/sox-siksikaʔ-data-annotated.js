94ba713ebcf74540ae34140c99b54c67bb0702b7
ssirrikh <13358109+Ssirrikh@users.noreply.github.com>
2025-01-24 12:22:16 -0800

    add stories of Coyote's Eyes, and Dove and Hummingbird

---
+++ source/story-0/sox-siksikaʔ-data-annotated.js
@@ -0,0 +1,111 @@
+
+let story_sox_siksikaʔ = {
+	audio : `source/story-0/sox-siksikaʔ-audio.wav`,
+	titleENG : `How Stinkbug Became Black`,
+	titleCHK : `Hawitmi Siksikaʔ Limeek’ataʔ`,
+	titleShortENG : `Black Stinkbug`,
+	titleShortCHK : `Siksikaʔ Limeek’ataʔ`,
+	timestampsBySentence : [
+		15.5,
+		`Sox himichtaʔ ʔoshto waʔaw shoyxow.`,
+		`Skunk made fire in a deep hole.`,
+		20.5,
+		`Miʔin ʔamaʔ panaataʔ Siksikaʔ.`,
+		`Then came Stinkbug.`,
+		25.0,
+		`Woodiyga, wiltaʔ Sox.`,
+		`“Dance”, said Skunk.`,
+		28.2,
+		`Namix, woodiyga. Miʔin gidwiyag ʔoshtow, haatimtaʔ Sox.`,
+		`“Friend, dance. And then go around the fire,” sang Skunk.`,
+		34.7,
+		`Miʔin ʔamaʔ Sox ʔutuytaʔ Siksikaʔan ʔoshtow.`,
+		`And then Skunk pushed Stinkbug in the fire.`,
+		40.0,
+		`Waʔ henew ʔadil’, Siksikaʔ t’uluntaʔ.`,
+		`Way down there, Stinkbug burned up.`,
+		44.5,
+		`Waʔ ʔamaʔ ʔadil’, ʔamaʔ ʔoshit shaapintaʔ.`,
+		`Then, way down there, the fire went out.`,
+		49.5,
+		`Miʔin ʔamaʔ Siksikaʔ tishtaʔ, meejinmi limeek’am.`,
+		`Then Stinkbug came out, and he turned black.`,
+		54.0,
+		`ʔohom’ may’ hudaʔan’ hawitmi hoyul’ Siksikaʔ, ch’uyuʔ daʔ t’uluntaʔ.`,
+		`We don’t know how Stinkbug was alive, even though he was burned.`
+	],
+	timestampsByClause : [
+		[
+			15.5,
+			`Sox himichtaʔ ʔoshto waʔaw shoyxow.`,
+			`Skunk made fire in a deep hole.`
+		],[
+			20.5,
+			`Miʔin ʔamaʔ panaataʔ Siksikaʔ.`,
+			`Then came Stinkbug.`
+		],[
+			25.0,
+			`Woodiyga, wiltaʔ Sox.`,
+			`“Dance”, said Skunk.`
+		],[
+			28.2,
+			`Namix, woodiyga. `,
+			`“Friend, dance.`,
+			30.3,
+			`Miʔin gidwiyag ʔoshtow, `,
+			`And then go around the fire,”`,
+			32.0,
+			`haatimtaʔ Sox.`,
+			`sang Skunk.`
+		],[
+			34.7,
+			`Miʔin ʔamaʔ Sox ʔutuytaʔ Siksikaʔan ʔoshtow.`,
+			`And then Skunk pushed Stinkbug in the fire.`
+		],[
+			40.0,
+			`Waʔ henew ʔadil’, `,
+			`Way down there,`,
+			41.7,
+			`Siksikaʔ t’uluntaʔ.`,
+			`Stinkbug burned up.`
+		],[
+			44.5,
+			`Waʔ ʔamaʔ ʔadil’, `,
+			`Then, way down there,`,
+			46.0,
+			`ʔamaʔ ʔoshit shaapintaʔ.`,
+			`the fire went out.`
+		],[
+			49.5,
+			`Miʔin ʔamaʔ Siksikaʔ tishtaʔ, `,
+			`Then Stinkbug came out,`,
+			51.6,
+			`meejinmi limeek’am.`,
+			`and he turned black.`
+		],[
+			54.0,
+			`ʔohom’ may’ hudaʔan’ hawitmi hoyul’ Siksikaʔ, `,
+			`We don’t know how Stinkbug was alive,`,
+			58.4,
+			`ch’uyuʔ daʔ t’uluntaʔ.`,
+			`even though he was burned.`
+		]
+	],
+	timestampsByWord : [
+		//
+	],
+	thumbnail : `source/story-0/sox-siksikaʔ-thumbnail-small.jpg`,
+	images : [
+		15.5,
+		`source/story-0/sox-siksikaʔ-photo-1.jpg`,
+		25.0,
+		`source/story-0/sox-siksikaʔ-photo-2.jpg`,
+		34.7,
+		`source/story-0/sox-siksikaʔ-photo-3.jpg`,
+		49.5,
+		`source/story-0/sox-siksikaʔ-photo-4.jpg`,
+		54.0,
+		`source/story-0/sox-siksikaʔ-photo-5.jpg`
+	]
+};
+	
---
+++ source/story-1/kaʔyuʔun-shashaʔ-data-annotated.js
@@ -0,0 +1,129 @@
+
+let story_kaʔyuʔun_shashaʔ = {
+	audio : `source/story-1/kaʔyuʔun-shashaʔ-audio.wav`,
+	titleENG : `Why Coyote's Eyes are the Best`,
+	titleCHK : `Hawitmi Kaʔyuʔun Shashaʔ Mich Gayis`,
+	titleShortENG : `Coyote's Eyes`,
+	titleShortCHK : `Kaʔyuʔun Shashaʔ`,
+	timestampsBySentence : [
+		4.0,
+		`Yeech’at Lopis ʔalit mich gayis shasham’.`,
+		`At first, Fish had the best eyes.`,
+		9.0,
+		`Kaʔyuʔ hoyochʔan’ meeji gay’si shashaaʔan, ʔamaʔ hoyochʔan’ gay’si hidyaʔan haʔan taw.`,
+		`Coyote wanted the best eyes, and he wanted everything to be the best.`,
+		17.5,
+		`Kaʔyuʔ mich mahmaʔach’.`,
+		`Coyote was no good.`,
+		20.5,
+		`Kaʔyuʔ tantaʔ ʔadil’ wakaayaw ʔamaa yoʔ ch’edmaxon’ wech’eeta hik’ey’hiy’.`,
+		`Coyote went down to the river and pretended to see a stick game.`,
+		29.0,
+		`Kaʔyuʔ wiltaʔ, Taʔishtaʔ naʔ wech’eeta hik’ey’hiy’.`,
+		`Coyote said, “I saw a stick game.”`,
+		34.7,
+		`Lopis wilit, ʔohom’ taʔishtaʔ naʔ wech’eeta’an hik’ey’hiy’, ʔamaʔ yoʔ wilit, ʔahmuch nim shashaʔ mich gayis! Wanwishxa mak’ shashaaʔan maagin.`,
+		`Fish said, “I don’t see a stick game,” and then said, “My eyes are supposed to be the best! Let’s trade eyes.”`,
+		49.0,
+		`Kaʔyuʔ ʔahmuch ch’edmat.`,
+		`Coyote pretended to think about it.`,
+		51.5,
+		`Miʔin Kaʔyuʔ wilit, Huuhuʔ. Mak’ wanwisheʔ maagin shashaaʔan.`,
+		`Then Coyote said, “Okay. We will trade our eyes.”`,
+		58.4,
+		`Kaʔyuʔ maaxit gay’si shashaaʔan.`,
+		`Coyote got the good eyes.`,
+		61.6,
+		`Miʔin Lopis maaxit bajxaʔli shashaaʔan.`,
+		`Then Fish got the bad eyes.`,
+		65.9,
+		`Lopis ohom’ naʔash taʔshal gay’si.`,
+		`Fish has not been able to see well since.`
+	],
+	timestampsByClause : [
+		[
+			4.0,
+			`Yeech’at Lopis ʔalit mich gayis shasham’.`,
+			`At first, Fish had the best eyes.`
+		],[
+			9.0,
+			`Kaʔyuʔ hoyochʔan’ meeji gay’si shashaaʔan,`,
+			`Coyote wanted the best eyes,`,
+			13.5,
+			`ʔamaʔ hoyochʔan’ gay’si hidyaʔan haʔan taw.`,
+			`and he wanted everything to be the best.`
+		],[
+			17.5,
+			`Kaʔyuʔ mich mahmaʔach’.`,
+			`Coyote was no good.`
+		],[
+			20.5,
+			`Kaʔyuʔ tantaʔ ʔadil’ wakaayaw`,
+			`Coyote went down to the river`,
+			23.5,
+			`ʔamaa yoʔ ch’edmaxon’ wech’eeta hik’ey’hiy’.`,
+			`and pretended to see a stick game.`
+		],[
+			29.0,
+			`Kaʔyuʔ wiltaʔ,`,
+			`Coyote said,`,
+			30.7,
+			`Taʔishtaʔ naʔ wech’eeta hik’ey’hiy’.`,
+			`“I saw a stick game.”`
+		],[
+			34.7,
+			`Lopis wilit,`,
+			`Fish said,`,
+			36.3,
+			`ʔohom’ taʔishtaʔ naʔ wech’eeta’an hik’ey’hiy’,`,
+			`“I don’t see a stick game,”`,
+			40.0,
+			`ʔamaʔ yoʔ wilit,`,
+			`and then said,`,
+			41.5,
+			`ʔahmuch nim shashaʔ mich gayis!`,
+			`“My eyes are supposed to be the best!`,
+			45.0,
+			`Wanwishxa mak’ shashaaʔan maagin.`,
+			`Let’s trade eyes.”`
+		],[
+			49.0,
+			`Kaʔyuʔ ʔahmuch ch’edmat.`,
+			`Coyote pretended to think about it.`
+		],[
+			51.5,
+			`Miʔin Kaʔyuʔ wilit,`,
+			`Then Coyote said,`,
+			53.2,
+			`Huuhuʔ. Mak’ wanwisheʔ maagin shashaaʔan.`,
+			`“Okay. We will trade our eyes.”`
+		],[
+			58.4,
+			`Kaʔyuʔ maaxit gay’si shashaaʔan.`,
+			`Coyote got the good eyes.`
+		],[
+			61.6,
+			`Miʔin Lopis maaxit bajxaʔli shashaaʔan.`,
+			`Then Fish got the bad eyes.`
+		],[
+			65.9,
+			`Lopis ohom’ naʔash taʔshal gay’si.`,
+			`Fish has not been able to see well since.`
+		]
+	],
+	timestampsByWord : [
+		//
+	],
+	thumbnail : `source/story-1/kaʔyuʔun-shashaʔ-thumbnail-small.jpg`,
+	images : [
+		4.0,
+		`source/story-1/kaʔyuʔun-shashaʔ-photo-1.jpg`,
+		20.5,
+		`source/story-1/kaʔyuʔun-shashaʔ-photo-2.jpg`,
+		49.0,
+		`source/story-1/kaʔyuʔun-shashaʔ-photo-3.jpg`,
+		61.6,
+		`source/story-1/kaʔyuʔun-shashaʔ-photo-4.jpg`
+	]
+};
+	
