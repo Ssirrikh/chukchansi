3ce316b094d74cf2050258b49c13631558ac978e
ssirrikh <13358109+Ssirrikh@users.noreply.github.com>
2025-01-27 11:03:26 -0800

    add/annotate shelel'-shilithanaʔ story

---
+++ source/story-3/shelel'-shilithanaʔ-data-annotated.js
@@ -0,0 +1,100 @@
+
+let story_shelel_shilithanaʔ = {
+	audio : `source/story-3/shelel'-shilithanaʔ-audio.mp3`,
+	titleENG : `A Rock To Be Jumped`,
+	titleCHK : `Shelel’ Shilithanaʔ`,
+	titleShortENG : `A Rock To Be Jumped`,
+	titleShortCHK : `Shelel’ Shilithanaʔ`,
+	timestampsBySentence : [
+		3.0,
+		`ʔooch’iy’ Nohʔoʔon Dach’aʔhiyaw Shelel’, ʔamaʔ hidʔan’ shelel’ nahniʔ yitshinil dadach’ sipin’.`,
+		`Near Bear Track Rock, there is another rock about five feet high.`,
+		0.0,
+		`Yokuch’ hik’eymi ʔamaa taʔan shil’t’eʔ sheleela.`,
+		`While playing, the people would jump the rock.`,
+		0.0,
+		`Miʔin daʔ yokuch’ wil’ lihmaʔiiwin’ ʔamaa.`,
+		`The people would go run toward it.`,
+		0.0,
+		`ʔamaʔ daʔ shelel’ wil’ bohloʔ hapt’is.`,
+		`Then the rock would grow a little.`,
+		0.0,
+		`Miʔin shilittaw, xishweʔ ʔamaamin wil’.`,
+		`When they jump over it, it would scratch them.`,
+		0.0,
+		`ʔaman noneeʔi ʔaman yoʔ mokeela wil’ demeenan’ shil’t’ayich’ daʔ sipin’.`,
+		`The men and women would try to jump higher.`,
+		0.0,
+		`Holoomun ʔamoogun shilittaw sheleela, dik’in ʔamoogun wil’ texeetan’.`,
+		`Every time they try to jump over the rock, their behinds would hurt.`,
+		0.0,
+		`Heddaʔ ʔamaa, nahniʔ ʔaman xayaataʔ peeli ʔamaa.`,
+		`It is still there, maybe they put a road there.`,
+		0.0,
+		`ʔamaʔ taʔach’.`,
+		`That is all of that.`
+	],
+	timestampsByClause : [
+		[
+			2.7,
+			`ʔooch’iy’ Nohʔoʔon Dach’aʔhiyaw Shelel’,`,
+			`Near Bear Track Rock,`,
+			6.8,
+			`ʔamaʔ hidʔan’ shelel’ nahniʔ yitshinil dadach’ sipin’.`,
+			`there is another rock about five feet high.`
+		],[
+			11.0,
+			`Yokuch’ hik’eymi ʔamaa taʔan shil’t’eʔ sheleela.`,
+			`While playing, the people would jump the rock.`
+		],[
+			16.0,
+			`Miʔin daʔ yokuch’ wil’ lihmaʔiiwin’ ʔamaa.`,
+			`The people would go run toward it.`
+		],[
+			20.0,
+			`ʔamaʔ daʔ shelel’ wil’ bohloʔ hapt’is.`,
+			`Then the rock would grow a little.`
+		],[
+			23.5,
+			`Miʔin shilittaw,`,
+			`When they jump over it,`,
+			25.2,
+			`xishweʔ ʔamaamin wil’.`,
+			`it would scratch them.`
+		],[
+			28.0,
+			`ʔaman noneeʔi ʔaman yoʔ mokeela`,
+			`The men and women`,
+            30.8,
+            `wil’ demeenan’ shil’t’ayich’ daʔ sipin’.`,
+            `would try to jump higher.`
+		],[
+			34.0,
+			`Holoomun ʔamoogun shilittaw sheleela,`,
+			`Every time they try to jump over the rock,`,
+			37.5,
+			`dik’in ʔamoogun wil’ texeetan’.`,
+			`their behinds would hurt.`
+		],[
+			41.0,
+			`Heddaʔ ʔamaa,`,
+			`It is still there,`,
+			43.7,
+			`nahniʔ ʔaman xayaataʔ peeli ʔamaa.`,
+			`maybe they put a road there.`
+		],[
+			47.3,
+			`ʔamaʔ taʔach’.`,
+			`That is all of that.`
+		]
+	],
+	timestampsByWord : [
+		//
+	],
+	thumbnail : `source/story-3/shelel'-shilithanaʔ-thumbnail-small.jpg`,
+	images : [
+		0.0,
+		`source/story-3/shelel'-shilithanaʔ-photo-1.jpg`
+	]
+};
+	
