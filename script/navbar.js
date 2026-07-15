1c51471a4668dd21778fa3aa328ca8d888915483
ssirrikh <13358109+Ssirrikh@users.noreply.github.com>
2026-07-15 11:55:16 -0700

    remove nav menu hover behavior to fix safari being unable to capture touch events

--- script/navbar.js
+++ script/navbar.js
@@ -16,28 +16,41 @@
     // selectors
     const EMBEDDED_CTX_SELECTOR = 'iframe, object[type="application/pdf"], embed[type="application/pdf"]';
 
-    // touchstart fires before mousemove, so block next mousemove if interaction already handled
-    let navDropdownBlockDoubleFire = false;
+    // // touchstart fires before mousemove, so block next mousemove if interaction already handled
+    // let navDropdownBlockDoubleFire = false;
 
-    // cross-platform hover/tap dropdown
-    window.addEventListener('mousemove', e => {
-        if (navDropdownBlockDoubleFire) {
-            navDropdownBlockDoubleFire = false;
-            return;
-        }
-        if (navMenu.contains(e.target)) {
-            navDropdown.style.visibility = 'visible';
-        } else {
-            navDropdown.style.visibility = 'hidden';
-        }
-    });
-    // allow touch devices to tap the menu icon to close the dropdown
-    navTarget.addEventListener('touchstart', e => {
+    // // cross-platform hover/tap dropdown
+    // window.addEventListener('mousemove', e => {
+    //     if (navDropdownBlockDoubleFire) {
+    //         navDropdownBlockDoubleFire = false;
+    //         return;
+    //     }
+    //     if (navMenu.contains(e.target)) {
+    //         navDropdown.style.visibility = 'visible';
+    //     } else {
+    //         navDropdown.style.visibility = 'hidden';
+    //     }
+    // });
+    // // allow touch devices to tap the menu icon to close the dropdown
+    // navTarget.addEventListener('touchstart', e => {
+    //     if (navDropdown.style.visibility === 'visible') {
+    //         navDropdown.style.visibility = 'hidden';
+    //         navDropdownBlockDoubleFire = true;
+    //     }
+    // });
+
+    // toggle menu on click/tap
+    const toggleNav = evt => {
         if (navDropdown.style.visibility === 'visible') {
-            navDropdown.style.visibility = 'hidden';
-            navDropdownBlockDoubleFire = true;
+            // nav menu already open => close if we click outside it, or explicitly click the toggle button
+            if (!navDropdown.contains(evt.target)) navDropdown.style.visibility = 'hidden';
+        } else {
+            // nav menu is closed => open it if we clicked the toggle button
+            if (navMenu.contains(evt.target)) navDropdown.style.visibility = 'visible';
         }
-    });
+    }
+    window.addEventListener('click', toggleNav);
+
     // close dropdown if user interacts with embedded browsing contexts, since they capture mousemove
     let embeddedContexts = document.querySelector(EMBEDDED_CTX_SELECTOR);
     if (embeddedContexts) embeddedContexts.addEventListener('mouseenter', e => {
