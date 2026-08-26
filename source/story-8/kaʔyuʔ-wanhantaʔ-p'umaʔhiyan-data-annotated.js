39d89dae2d8d146fcbb9cec14895fef52da5d315
ssirrikh <13358109+Ssirrikh@users.noreply.github.com>
2026-08-26 12:46:20 -0700

    add stories 5-8: ambitious snake, boys become quail, coyote and bear swing, and coyote gets a beak

---
+++ source/story-8/kaʔyuʔ-wanhantaʔ-p'umaʔhiyan-data-annotated.js
@@ -0,0 +1,230 @@
+let story_kaʔyuʔ_wanhantaʔ_pumaʔhiyan = {
+    "thumbnail": "source/story-8/kaʔyuʔ-wanhantaʔ-p'umaʔhiyan-thumbnail.jpg",
+    "audio": "source/story-8/kaʔyuʔ-wanhantaʔ-p'umaʔhiyan-audio.mp3",
+    "titleENG": "Coyote Is Given a Beak",
+    "titleCHK": "Kaʔyuʔ Wanhantaʔ P’umaʔhiyan",
+    "titleShortENG": "Coyote's Beak",
+    "titleShortCHK": "Kaʔyuʔ Wanhantaʔ P’umaʔhiyan",
+    "timestampsBySentence":
+    [
+        0.0,
+        "Kaʔyuʔ Wanhantaʔ P’umaʔhiyan",
+        "Coyote Is Given a Beak",
+        3.5,
+        "Bajxaʔli Doshhantaʔ",
+        "He was told something bad",
+        6.3,
+        "Miʔin Kaʔyuʔ hewetmeewetaʔ.",
+        "There was Coyote walking along.",
+        9.3,
+        "Taʔishtaʔ ch’enbayi hitwash k’otiiʔin p’umaʔhiya’.",
+        "He saw a bird with a large beak.",
+        15.6,
+        "Heleytaʔ deenaʔan ʔam p’umaʔhiyaw.",
+        "He was carrying a wild potato in his beak.",
+        19.3,
+        "Kaʔyuʔ liptaʔ ʔam wayaach’i deenaʔan.",
+        "Coyote watched him dig potatoes.",
+        23.2,
+        "Miʔin tantaʔ ʔamaa, yatach’ ʔam.",
+        "Then he went there to speak to him.",
+        26.7,
+        "Kaʔyuʔ benettaʔ ʔam, Haʔan daʔ maʔ wayaʔan’?",
+        "Coyote asked him, “What are you digging?”",
+        30.5,
+        "Ch’enbay’ wiltaʔ, Wayaʔan’ naʔ deenaʔan, xataʔhiy’ nim.",
+        "The bird said, “I’m digging some wild potatoes to eat.”",
+        35.6,
+        "Kaʔyuʔ ʔam wiltaʔ, Yashanʔan’ naʔ min neyeh p’umaʔhiya,",
+        "Coyote said to him, “I wish for a beak like yours,",
+        41,
+        "miʔin naʔ naʔash waayal deenaʔan,",
+        "then I could dig wild potatoes,",
+        45.0,
+        "halaadaʔ gadayʔan’ naʔ duʔ.",
+        "because I’m hungry.”",
+        48.3,
+        "Benettaʔ ch’enbayi, Huyuch’ maʔ naʔash diʔshal nan,",
+        "He asked the bird, “Could you fix me",
+        53.6,
+        "miʔin naʔ naʔash xoch’ooyow waayal?",
+        "so I could dig in the ground?”",
+        57.3,
+        "ʔamaʔ ch’enbay’ wiltaʔ, Nahniʔ naʔ naʔash diʔshal mam",
+        "The bird said, “Maybe I could fix you",
+        62.5,
+        "t’ee maʔ maaxeʔ k’otiiʔin t’apaapaych’i sheleela.",
+        "if you get me a large flat rock.”",
+        67.5,
+        "ʔohyotaʔ ʔamak’ sheleela xanich’tayich’ Kaʔyuʔun shamaaʔan.",
+        "They looked for a rock to sharpen Coyote’s mouth.",
+        74,
+        "Miʔin ʔamaʔ ch’enbay’ bok’toʔ hoy’li.",
+        "The bird soon found it.",
+        77.5,
+        "Doshtoʔ Kaʔyuʔun, Holoshga,",
+        "He told Coyote, “Sit down,",
+        80.8,
+        "miʔin naʔ naʔash potol min shamaaʔan.",
+        "so I could reach your mouth.”",
+        84.2,
+        "Kaʔyuʔ holoshtaʔ ʔamaa.",
+        "Coyote sat down there.",
+        86.9,
+        "Miʔin ch’enbay’ wostoʔ ʔam yeech’at ʔujukshulaw,",
+        "The bird hit him once on his jaw,",
+        91.4,
+        "ʔooch’iy’ ʔam tuk’aw.",
+        "near his ears.",
+        93.4,
+        "Kaʔyuʔ sawtaʔ.",
+        "Coyote hollered.",
+        95.2,
+        "Waxilmi wiltaʔ, Tixit!",
+        "He cried and said, “It hurts!”",
+        97.6,
+        "Miʔin lihimtaʔ.",
+        "Then he ran away.",
+        99.6,
+        "ʔamaʔ taʔach’.",
+        "That’s the end."
+    ],
+    "timestampsByClause":
+    [
+        [
+            0.0,
+            "Kaʔyuʔ Wanhantaʔ P’umaʔhiyan",
+            "Coyote Is Given a Beak"
+        ],
+        [
+            3.5,
+            "Bajxaʔli Doshhantaʔ",
+            "He was told something bad"
+        ],
+        [
+            6.3,
+            "Miʔin Kaʔyuʔ hewetmeewetaʔ.",
+            "There was Coyote walking along."
+        ],
+        [
+            9.3,
+            "Taʔishtaʔ ch’enbayi hitwash k’otiiʔin p’umaʔhiya’.",
+            "He saw a bird with a large beak."
+        ],
+        [
+            15.6,
+            "Heleytaʔ deenaʔan ʔam p’umaʔhiyaw.",
+            "He was carrying a wild potato in his beak."
+        ],
+        [
+            19.3,
+            "Kaʔyuʔ liptaʔ ʔam wayaach’i deenaʔan.",
+            "Coyote watched him dig potatoes."
+        ],
+        [
+            23.2,
+            "Miʔin tantaʔ ʔamaa, yatach’ ʔam.",
+            "Then he went there to speak to him."
+        ],
+        [
+            26.7,
+            "Kaʔyuʔ benettaʔ ʔam, Haʔan daʔ maʔ wayaʔan’?",
+            "Coyote asked him, “What are you digging?”"
+        ],
+        [
+            30.5,
+            "Ch’enbay’ wiltaʔ, Wayaʔan’ naʔ deenaʔan, xataʔhiy’ nim.",
+            "The bird said, “I’m digging some wild potatoes to eat.”"
+        ],
+        [
+            35.6,
+            "Kaʔyuʔ ʔam wiltaʔ, Yashanʔan’ naʔ min neyeh p’umaʔhiya,",
+            "Coyote said to him, “I wish for a beak like yours,"
+        ],
+        [
+            41,
+            "miʔin naʔ naʔash waayal deenaʔan,",
+            "then I could dig wild potatoes,"
+        ],
+        [
+            45.0,
+            "halaadaʔ gadayʔan’ naʔ duʔ.",
+            "because I’m hungry.”"
+        ],
+        [
+            48.3,
+            "Benettaʔ ch’enbayi, Huyuch’ maʔ naʔash diʔshal nan,",
+            "He asked the bird, “Could you fix me"
+        ],
+        [
+            53.6,
+            "miʔin naʔ naʔash xoch’ooyow waayal?",
+            "so I could dig in the ground?”"
+        ],
+        [
+            57.3,
+            "ʔamaʔ ch’enbay’ wiltaʔ, Nahniʔ naʔ naʔash diʔshal mam",
+            "The bird said, “Maybe I could fix you"
+        ],
+        [
+            62.5,
+            "t’ee maʔ maaxeʔ k’otiiʔin t’apaapaych’i sheleela.",
+            "if you get me a large flat rock.”"
+        ],
+        [
+            67.5,
+            "ʔohyotaʔ ʔamak’ sheleela xanich’tayich’ Kaʔyuʔun shamaaʔan.",
+            "They looked for a rock to sharpen Coyote’s mouth."
+        ],
+        [
+            74,
+            "Miʔin ʔamaʔ ch’enbay’ bok’toʔ hoy’li.",
+            "The bird soon found it."
+        ],
+        [
+            77.5,
+            "Doshtoʔ Kaʔyuʔun, Holoshga,",
+            "He told Coyote, “Sit down,"
+        ],
+        [
+            80.8,
+            "miʔin naʔ naʔash potol min shamaaʔan.",
+            "so I could reach your mouth.”"
+        ],
+        [
+            84.2,
+            "Kaʔyuʔ holoshtaʔ ʔamaa.",
+            "Coyote sat down there."
+        ],
+        [
+            86.9,
+            "Miʔin ch’enbay’ wostoʔ ʔam yeech’at ʔujukshulaw,",
+            "The bird hit him once on his jaw,"
+        ],
+        [
+            91.4,
+            "ʔooch’iy’ ʔam tuk’aw.",
+            "near his ears."
+        ],
+        [
+            93.4,
+            "Kaʔyuʔ sawtaʔ.",
+            "Coyote hollered."
+        ],
+        [
+            95.2,
+            "Waxilmi wiltaʔ, Tixit!",
+            "He cried and said, “It hurts!”"
+        ],
+        [
+            97.6,
+            "Miʔin lihimtaʔ.",
+            "Then he ran away."
+        ],
+        [
+            99.6,
+            "ʔamaʔ taʔach’.",
+            "That’s the end."
+        ]
+    ]
+}
